1a1f6f11e78d321bff6198f7a18339cb32d487c8
ryowakitazawa82 <ryowa.kitazawa@gmail.com>
2022-07-31 01:04:01 +0900

    一通り実装

--- MyCoreData/Persistent.swift
+++ MyCoreData/Persistent.swift
@@ -5,4 +5,19 @@
 //  Created by 北沢　亮和 on 2022/07/31.
 //
 
-import Foundation
+import CoreData
+
+struct PersistenceController {
+    let container: NSPersistentContainer
+    
+    init() {
+        container = NSPersistentContainer(name: "MyCoreData")
+        
+        container.loadPersistentStores(completionHandler: {
+            (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error)")
+            }
+        })
+    }
+}
