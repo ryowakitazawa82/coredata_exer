1683d5a8752c1b5114b1088b0c4cd574aa48b4fc
ryowakitazawa82 <ryowa.kitazawa@gmail.com>
2022-07-31 00:17:08 +0900

    Initial Commit

---
+++ MyCoreData/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyCoreData
+//
+//  Created by 北沢　亮和 on 2022/07/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyCoreData/MyCoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCoreDataApp.swift
+//  MyCoreData
+//
+//  Created by 北沢　亮和 on 2022/07/31.
+//
+
+import SwiftUI
+
+@main
+struct MyCoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
